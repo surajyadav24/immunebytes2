bd6d4887200345ae714d45c61d80202f54b2cfe0
ChetnaDigitalMolecule <chetnadigitalmolecule@gmail.com>
2024-11-09 17:29:56 +0530

    DB

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,5 @@
 import express from "express"
 import cors from 'cors'
-// import cookieParser from "cookie-parser"
 
 const app = express()
 
@@ -12,7 +11,6 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
-// app.use(cookieParser())
 
 
 

5692d5e630b82e88cfb073ba866cd3b29114fd5e
ChetnaDigitalMolecule <chetnadigitalmolecule@gmail.com>
2024-11-09 17:27:52 +0530

    DB

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import express from "express"
+import cors from 'cors'
+// import cookieParser from "cookie-parser"
 
-function app() {
-  return (
-    <div>app</div>
-  )
-}
+const app = express()
 
-export default app
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+// app.use(cookieParser())
+
+
+
+
+
+export { app }

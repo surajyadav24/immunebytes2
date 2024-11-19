27b10bb130918b707359f240c7c0010bef3b1f68
suraj kumar yadav <7suraj8yadav6@gmail.com>
2024-11-19 15:00:15 +0530

    audit section of dashboard

--- immunebytes/src/components/Audit-Progress/index.jsx
+++ immunebytes/src/components/Audit-Progress/index.jsx
@@ -1,16 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import ProgressBar from '../Progressbar'
 import "./style.css"
 function AuditProgress() {
+
+  const [percentages, setPercentages] = useState({
+    critical: 0,
+    high: 0,
+    medium: 0,
+    low: 0,
+    informational: 0,
+  });
+
+  useEffect(() => {
+    const fetchSeverityData = async () => {
+      try {
+        const response = await axios.post('/api/v1/users/getseverity');
+        if (response.data && response.data.data && response.data.data.percentages) {
+          setPercentages(response.data.data.percentages); // Use percentages from the response
+        } else {
+          console.error('Invalid response data:', response.data);
+        }
+      } catch (error) {
+        console.error('Error fetching severity data', error);
+      }
+    };
+
+    fetchSeverityData();
+  }, []);
+
+
   return (
 <>
 <div className="audit-progress">
     <h4>Severity Found</h4>
-    <ProgressBar label="Critical" color="#ff4d4d" percentage={58} />
-    <ProgressBar label="High" color="#ff9966" percentage={70} />
-    <ProgressBar label="Medium" color="#ffcc66" percentage={30} />
-    <ProgressBar label="Low" color="#66ff66" percentage={60} />
-    <ProgressBar label="Informational" color="#66cc66" percentage={90} />
+    <ProgressBar label="Critical" color="#ff4d4d" percentage={percentages.critical} />
+            <ProgressBar label="High" color="#ff9966" percentage={percentages.high} />
+            <ProgressBar label="Medium" color="#ffcc66" percentage={percentages.medium} />
+            <ProgressBar label="Low" color="#66ff66" percentage={percentages.low} />
+            <ProgressBar label="Informational" color="#66cc66" percentage={percentages.informational} />
   </div>
 </> 
   )

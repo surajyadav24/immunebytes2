14af1e8b422fd7e3d4ece14243226676a6ee9784
suraj kumar yadav <7suraj8yadav6@gmail.com>
2024-11-27 16:37:10 +0530

    url update

--- immunebytes/src/Pages/user/Login/index.jsx
+++ immunebytes/src/Pages/user/Login/index.jsx
@@ -16,7 +16,7 @@ function LoginForm() {
 
     try {
       const response = await axios.post(
-        '/api/v1/users/login',
+        'https://immunebytes2-7w2a-a9iyijvcd-surajyadav24s-projects.vercel.app/login',
         { username, password },
         { withCredentials: true },
        

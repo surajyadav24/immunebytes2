eb5e5a0c61bf093032627e4c8d6553b93723fe8e
ChetnaDigitalMolecule <chetnadigitalmolecule@gmail.com>
2024-11-28 12:10:15 +0530

    DB-connection error

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,10 @@
 import dotenv from "dotenv"
 import { app } from "./app.js"
 import connectDB from "./src/db/index.js"
+
+
 dotenv.config({
-    path:'./env'
+    path:'./.env'
 })
 
 
@@ -11,6 +13,8 @@ connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+        console.log("MONGODB_URI:", process.env.MONGODB_URI);
+
     })
 })
 .catch((err) => {

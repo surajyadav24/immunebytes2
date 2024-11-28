0d65f83d91c8042fb13e66aeb8cebd19236cacc4
suraj kumar yadav <7suraj8yadav6@gmail.com>
2024-11-28 14:58:52 +0530

    production

--- Backend/app.js
+++ Backend/app.js
@@ -31,7 +31,13 @@ axios.interceptors.response.use(
       return Promise.reject(error);
     }
   );
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
   
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+  }
 
 app.get('/',(req,res)=>{
     res.send("hello")

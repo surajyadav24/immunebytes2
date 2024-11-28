177841c081db04306991fe8af99987bcc9b0883a
suraj kumar yadav <7suraj8yadav6@gmail.com>
2024-11-28 12:11:07 +0530

    Revert "backend connection"
    This reverts commit eb63467c7750a746102e96ac9f2fe1064b6a550f.

--- Backend/app.js
+++ Backend/app.js
@@ -6,15 +6,7 @@ import axios from "axios"
 
 const app = express()
 
-app.use(cors(
-{
-  origin:["https://immunebytes2-7w2a-a9iyijvcd-surajyadav24s-projects.vercel.app/"],
-  methods:["POST"],
-  credentials:true,
-
-}
-
-));
+app.use(cors());
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))

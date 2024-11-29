d0185d643d6ab39d5acb422a5e2beaa50c99d682
suraj kumar yadav <7suraj8yadav6@gmail.com>
2024-11-29 17:08:05 +0530

    Faq changes

--- immunebytes/src/components/FAQ/index.jsx
+++ immunebytes/src/components/FAQ/index.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import "./style.css"
-import downarrow from "../../assets/images/services-main/down-arrow.svg"
-import rightarrow from "../../assets/images/services-main/right-arrow.svg"
+import "./style.css";
+import downarrow from "../../assets/images/services-main/down-arrow.svg";
+import rightarrow from "../../assets/images/services-main/right-arrow.svg";
+
 const FAQ = () => {
   const [openIndex, setOpenIndex] = useState(null);
 
@@ -30,22 +31,34 @@ const FAQ = () => {
   ];
 
   return (
-    <div className="  p-6">
-      <h2 className="text-2xl font-bold text-center text-pink-500 mb-6">
+    <div className="p-6 faq-container">
+      <h2 className="text-2xl font-bold text-center heading mb-6 pb-6">
         FAQs
       </h2>
       <div className="space-y-4">
         {faqs.map((faq, index) => (
           <div
             key={index}
-            className="faq-btn  border rounded-lg overflow-hidden"
+            className="faq-btn border rounded-lg overflow-hidden"
           >
             <button
               onClick={() => toggleFAQ(index)}
-              className="w-full flex justify-between items-center bg-gray-800 text-white px-4 py-3 text-left"
+              className="w-full flex justify-between items-center  px-4 py-3 text-left"
             >
-              <span>{faq.question}</span>
-              <span>{openIndex === index ? <img src={downarrow} alt="" /> :<img src={rightarrow} alt="" />}</span>
+              <span
+                className={`${
+                  openIndex === index ? "text-pink-500" : "text-white"
+                }`}
+              >
+                {faq.question}
+              </span>
+              <span>
+                {openIndex === index ? (
+                  <img src={downarrow} alt="" />
+                ) : (
+                  <img src={rightarrow} alt="" />
+                )}
+              </span>
             </button>
             {openIndex === index && (
               <div className="answer bg-gray-900 text-gray-300 px-4 py-3">

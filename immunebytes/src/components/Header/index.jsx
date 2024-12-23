6b7d99d89e2e2dff10fbecebded50a64cef26930
suraj kumar yadav <7suraj8yadav6@gmail.com>
2024-12-23 18:05:24 +0530

    menu updated

--- immunebytes/src/components/Header/index.jsx
+++ immunebytes/src/components/Header/index.jsx
@@ -1,110 +1,171 @@
 import React, { useState } from "react";
 import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Logo from '../../assets/images/logos/Logo.svg';
-import Formpopup from '../Formpopup';
-import dropdown from '../../assets/images/dropdown.svg'
+import Logo from "../../assets/images/logos/Logo.svg";
+import Formpopup from "../Formpopup";
+import dropdown from "../../assets/images/dropdown.svg";
+import defi from "../../assets/images/header/defi.svg"
+import nft from "../../assets/images/header/crypto.svg"
+import token from "../../assets/images/header/token.svg"
+import dapp from "../../assets/images/header/dapp.svg"
+
+
+
+function DropdownMenu({ title, links }) {
+  return (
+    <li className="nav-item dropdown">
+      <a href="#" className="nav-link text-white">
+        {title} <span><img src={dropdown} alt="dropdown icon" /></span>
+      </a>
+      <ul className="dropdown-menu">
+        {links.map((link, index) => (
+          <li key={index} className="nav-item dropdown">
+            <a href={link.href} className="dropdown-item">
+              {link.label}
+            </a>
+            {link.subLinks && (
+              <ul className="dropdown-menu sub-menu">
+                {/* Custom sublinks label */}
+                {link.subLinksLabel && (
+                  <span className="dropdown-header">{link.subLinksLabel}</span>
+                )}
+                {link.subLinks.map((subLink, subIndex) => (
+                  <li key={subIndex}>
+                    <a href={subLink.href} className="dropdown-item">
+                      {subLink.icon && <img src={subLink.icon} alt={subLink.label} className="menu-icon" />}
+                      {subLink.label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
+      </ul>
+    </li>
+  );
+}
 
 function Header() {
-  const [isOpen, setIsOpen] = useState(false); // State to track menu open/close
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false); // For Services dropdown (desktop)
-  const [isMobileDropdownOpen, setIsMobileDropdownOpen] = useState(false); // For Services dropdown (mobile)
+  const [isOpen, setIsOpen] = useState(false); // State for mobile menu
 
   const toggleMenu = () => {
     setIsOpen(!isOpen); // Toggle menu on click
   };
 
-  const toggleDropdown = (state) => {
-    setIsDropdownOpen(state);
-  };
-
-  const toggleMobileDropdown = () => {
-    setIsMobileDropdownOpen(!isMobileDropdownOpen);
-  };
-
+  const dropdownLinks = [
+    {
+      href: "/smartcontract",
+      label: "Smart Contract",
+      subLinksLabel: "By Type",  
+      subLinks: [
+        { href: "/new-menu1", label: "Defi", icon:defi },
+        { href: "/new-menu2", label: "Nft", icon: nft },
+        { href: "/new-menu3", label: "Token", icon: token },
+        { href: "/new-menu4", label: "Dapp", icon: dapp },
+      ],
+      subLinksLabelservice: "By Service",  
+      subLinksservice: [
+        { href: "/new-menu1", label: "Defi", icon:defi },
+        { href: "/new-menu2", label: "Nft", icon: nft },
+        { href: "/new-menu3", label: "Token", icon: token },
+        { href: "/new-menu4", label: "Dapp", icon: dapp },
+      ],
+    },
+    {
+      href: "/penetration-testing",
+      label: "Penetration Testing",
+    },
+    {
+      href: "/blockchainaudit",
+      label: "Blockchain Audit",
+    },
+  ];
+  
   return (
-    <>
-      <header className="text-white p-3">
-        <div className="container d-flex justify-content-between align-items-center p-0">
-          <div className="h3 font-weight-bold logo">
-            <a href="/">
-              <img src={Logo} alt="" />
-            </a>
-          </div>
-
-          {/* Desktop Nav: Hidden on mobile/tablet */}
-          <nav className="desktop-nav d-none d-lg-block">
-            <ul className="nav">
-              <li
-                className="nav-item dropdown"
-                onMouseEnter={() => toggleDropdown(true)}
-                onMouseLeave={() => toggleDropdown(false)}
-              >
-                <a href="#" className="nav-link text-white">Services <span><img src={dropdown} alt="" /></span></a>
-                {isDropdownOpen && (
-                  <ul className="dropdown-menu">
-                    <li><a href="/smartcontract" className="dropdown-item">Smart Contract</a></li>
-                    <li><a href="/penetration-testing" className="dropdown-item">Penetration Testing</a></li>
-                    <li><a href="/blockchainaudit" className="dropdown-item">Blockchain Audit</a></li>
-                  </ul>
-                )}
-              </li>
-              <li className="nav-item">
-                <a href="/portfolio" className="nav-link text-white">Portfolio</a>
-              </li>
-              <li className="nav-item">
-                <a href="/about" className="nav-link text-white">About Us</a>
-              </li>
-              <li className="nav-item">
-                <a href="/blog" className="nav-link text-white">Blog</a>
-              </li>
-            </ul>
-          </nav>
-
-          {/* Request Button */}
-          <div className="desktop-btn-request">
-            <Formpopup />
-          </div>
-
-          {/* Hamburger Menu for Mobile & Tablet */}
-          <div className="hamburger d-lg-none" onClick={toggleMenu}>
-            <span className="bar"></span>
-            <span className="bar"></span>
-            <span className="bar"></span>
-          </div>
+    <header className="text-white p-3">
+      <div className="container d-flex justify-content-between align-items-center p-0">
+        <div className="h3 font-weight-bold logo">
+          <a href="/">
+            <img src={Logo} alt="Logo" />
+          </a>
         </div>
 
-        {/* Mobile/Tablet Menu */}
-        {isOpen && (
-          <nav className="mobile-nav d-lg-none">
-            <ul className="nav flex-column">
-              <li className="nav-item">
-                <div className="nav-link text-white" onClick={toggleMobileDropdown}>Services</div>
-                {isMobileDropdownOpen && (
-                  <ul className="dropdown-menu">
-                    <li><a href="/smartcontract" className="dropdown-item">Smart Contract</a></li>
-                    <li><a href="/penetration-testing" className="dropdown-item">Penetration Testing</a></li>
-                    <li><a href="/blockchainaudit" className="dropdown-item">Blockchain Audit</a></li>
-                  </ul>
-                )}
-              </li>
-              <li className="nav-item">
-                <a href="/portfolio" className="nav-link text-white">Portfolio</a>
-              </li>
-              <li className="nav-item">
-                <a href="/about-us" className="nav-link text-white">About Us</a>
-              </li>
-              <li className="nav-item">
-                <a href="/blog" className="nav-link text-white">Blog</a>
-              </li>
-              <li className="nav-item">
-                <Formpopup />
-              </li>
-            </ul>
-          </nav>
-        )}
-      </header>
-    </>
+        {/* Desktop Navigation */}
+        <nav className="desktop-nav d-none d-lg-block">
+          <ul className="nav">
+            <DropdownMenu title="Services" links={dropdownLinks} />
+            <li className="nav-item">
+              <a href="/portfolio" className="nav-link text-white">
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="/about" className="nav-link text-white">
+                About Us
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="/blog" className="nav-link text-white">
+                Blog
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Request Button */}
+        <div className="desktop-btn-request">
+          <Formpopup />
+        </div>
+
+        {/* Hamburger Menu for Mobile */}
+        <div className="hamburger d-lg-none" onClick={toggleMenu}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+      </div>
+
+      {/* Mobile Navigation */}
+      {isOpen && (
+        <nav className="mobile-nav d-lg-none">
+          <ul className="nav flex-column">
+            <li className="nav-item">
+              <a href="/services" className="nav-link text-white">
+                Services
+              </a>
+              <ul className="dropdown-menu">
+                {dropdownLinks.map((link, index) => (
+                  <li key={index}>
+                    <a href={link.href} className="dropdown-item">
+                      {link.label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a href="/portfolio" className="nav-link text-white">
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="/about" className="nav-link text-white">
+                About Us
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="/blog" className="nav-link text-white">
+                Blog
+              </a>
+            </li>
+            <li className="nav-item">
+              <Formpopup />
+            </li>
+          </ul>
+        </nav>
+      )}
+    </header>
   );
 }
 

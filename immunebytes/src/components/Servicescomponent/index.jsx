14586bf4f0930bef3b7510e5364df5650203f3f6
suraj kumar yadav <7suraj8yadav6@gmail.com>
2025-01-08 17:56:56 +0530

    our services changes

--- immunebytes/src/components/Servicescomponent/index.jsx
+++ immunebytes/src/components/Servicescomponent/index.jsx
@@ -8,14 +8,14 @@ const ServicesComponent = ({ heading, paragraphtext, imageSrc, className }) => {
       <div className="container-fluid">
   
         <div className="row">
-          <div className="col-lg-7 md-6 col-sm-6 col-xs-6 left-serv">
+          <div className="col-lg-7 md-6 col-sm-8 col-xs-8 left-serv">
             <div className="service-discription">
               <h2>{heading}</h2>
               <p>{paragraphtext}</p>
               <PrimaryBtn text="View More" />
             </div>
           </div>
-          <div className="col-lg-5 md-6 col-sm-6 col-xs-6 right-serv">
+          <div className="col-lg-5 md-6 col-sm-4 col-xs-4 right-serv">
             <div className="service-img">
               <img src={imageSrc} alt="Services" className="services-image" />
             </div>

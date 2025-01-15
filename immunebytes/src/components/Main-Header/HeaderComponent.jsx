013c392c977aef30927bb2945b1744ae5d01812c
suraj kumar yadav <7suraj8yadav6@gmail.com>
2025-01-15 18:27:08 +0530

    tab mobile changes

--- immunebytes/src/components/Main-Header/HeaderComponent.jsx
+++ immunebytes/src/components/Main-Header/HeaderComponent.jsx
@@ -6,7 +6,8 @@ import Formpopup from "../Formpopup"
 export default function HeaderComponent() {
   return (
     <div>
-      <header className="h-16 text-[15px] fixed inset-0 flex-center  shadow">
+<div className="container">
+<header className="h-16 text-[15px] fixed inset-0 flex-center  shadow">
         <nav className="px-3.5 flex-center-between w-full max-w-7xl mx-auto">
           {/* Logo Section */}
           <div className="flex-center relative logo-wrapper-main">
@@ -34,6 +35,7 @@ export default function HeaderComponent() {
           </div>
         </nav>
       </header>
+</div>
     </div>
   );
 }
